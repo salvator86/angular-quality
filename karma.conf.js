af15fade740d19c58721224c0519d8fb77d72aa6
Oleksandr Chukh <ochukh@softserveinc.com>
2025-03-24 17:24:14 +0200

    remove test artefacts

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,34 @@
 module.exports = function (config) {
   config.set({
-    reporters: ["progress", "junit"],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
+    ],
+    client:{
+      clearContext: false
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['progress', 'junit'],
     junitReporter: {
-      outputDir: "test-results",
-      outputFile: "junit.xml",
+      outputDir: 'test-results',
+      outputFile: 'junit.xml',
       useBrowserName: false
-    }
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
   });
 };
