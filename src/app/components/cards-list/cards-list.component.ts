3ac9bdfa880b11d3bf5f5e47a97282a72ae74142
Oleksandr Chukh <ochukh@softserveinc.com>
2025-03-24 14:51:55 +0200

    add linter report

---
+++ src/app/components/cards-list/cards-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-list',
+  imports: [],
+  templateUrl: './cards-list.component.html',
+  styleUrl: './cards-list.component.scss'
+})
+export class CardsListComponent {
+
+}
